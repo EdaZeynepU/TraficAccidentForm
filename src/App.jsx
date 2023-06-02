b764421aaf446c32539c600a0890c58277264b48
EdaZeynepU <72568009+EdaZeynepU@users.noreply.github.com>
2023-06-02 03:13:24 +0300

    adding form
    forms (3 to 6) are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
+import Form3 from './Form3';
 import Form from "./Form4to6";
+import { Container,Row,Col } from "reactstrap";
 
 function App() {
 
   return (
-    <div className='app'>
-      <Form/>
-    </div>
+    <Container>
+      <Row>
+        <Col xs="12">
+        <Form3/>
+        </Col>
+        <Col xs="12" md="6">
+          <h1>Driver A</h1>
+<Form driver={"A"}/>
+          </Col>
+        <Col xs="12" md="6">
+        <h1>Driver B</h1>
+
+        <Form driver={"B"}/>
+
+          </Col>
+      </Row>
+    </Container>
   )
 }
 
---
+++ src/Form3.jsx
@@ -0,0 +1,48 @@
+// import { useFormik } from "formik";
+import {useState} from "react";
+import { Form, Input, Button,Badge, ButtonGroup } from "reactstrap";
+// Label,Badge, FormGroup,
+
+
+function Form3() {
+    const [witnesses, setWitnesses] = useState(1);
+    const [witnessesArray, setWitnessesArray] = useState([]); //stores witnesses values
+
+    const addNewWitness = () => {
+      setWitnesses(witnesses + 1);
+    };
+  
+    const handleSubmit = (e) => {
+        const updatedWitnessesArray = [];
+        for (let i = 0; i < e.target.children.length - 1; i++) {
+          updatedWitnessesArray.push(e.target.children[i].value);
+        }
+        setWitnessesArray(updatedWitnessesArray);
+        e.preventDefault();
+      };
+    
+    const renderWitnesses = () => {
+      const witnessesArray = [];
+      for (let i = 0; i < witnesses; i++) {
+        witnessesArray.push(<Input key={i} placeholder="full name, adress, phone number"/>);
+      }
+      return witnessesArray;
+    };
+
+  return (<div>
+    <h3>
+          <Badge>3</Badge> Witnesses
+    </h3>
+  <Form onSubmit={handleSubmit}>
+    {renderWitnesses()}
+    <ButtonGroup>
+         <Button type="submit">Submit</Button>
+    <Button onClick={addNewWitness}>Add Witness</Button>
+    </ButtonGroup>
+   
+  </Form>
+  Witnesses: {witnessesArray.join(", ")}
+</div>)
+}
+
+export default Form3;
